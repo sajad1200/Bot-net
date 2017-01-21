98d6bddbc16a8d1f9ea5524317987b2b8dd0ee4e
sajad1200 <sego.sego1122@gmail.com>
2017-01-21 15:36:27 +0300

    Bot-Net

--- libs/JSON.lua
+++ libs/JSON.lua
@@ -2,7 +2,7 @@
 --
 -- Simple JSON encoding and decoding in pure Lua.
 --
--- Copyright 2010-2014 Jeffrey Friedl
+-- Copyright 2010-2014 Sajad Aliraqe
 -- http://regex.info/blog/
 --
 -- Latest version: http://regex.info/blog/lua/json
