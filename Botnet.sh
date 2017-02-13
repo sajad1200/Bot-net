e78478c1060a1b3b0d0b32e4ce34118a933419a4
sajad1200 <sego.sego1122@gmail.com>
2017-02-13 14:12:47 +0300

    Bot-net v.2

--- Botnet.sh
+++ Botnet.sh
@@ -5,15 +5,17 @@ cd $HOME/Bot-net
 
 install() {
 	    cd tg
-		sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-		sudo apt-get install g++-4.7 c++-4.7
-		sudo apt-get update		
-                sudo apt-get upgrade
-		sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
-		sudo apt-get install lua-lgi
-		sudo apt-get install libnotify-dev
-		sudo apt-get install screen
-		sudo apt-get install tmux
+		sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+		sudo apt-get install g++-4.7 -y c++-4.7 -y
+		sudo apt-get update
+		sudo apt-get upgrade
+		sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
+		sudo apt-get install screen -y
+		sudo apt-get install tmux -y
+		sudo apt-get install libstdc++6 -y
+		sudo apt-get install lua-lgi -y
+		sudo apt-get install libnotify-dev -y
+		sudo service redis-server restart
 		wget https://valtman.name/files/telegram-cli-1222
 		mv telegram-cli-1222 tgcli
 		chmod +x tgcli
