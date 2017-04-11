ff56cf08882bd8ff6f9801db97ab4d56b4d51e6c
sajad1200 <sego.sego1122@gmail.com>
2017-04-11 11:23:52 +0300

    Botnet v.3

--- tg/tdcli.lua
+++ tg/tdcli.lua
@@ -1,19 +1,17 @@
+
 --[[
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
-
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
    MA 02110-1301, USA.
-
 ]]--
 
 -- Vector example form is like this: {[0] = v} or {v1, v2, v3, [0] = v}
@@ -1339,7 +1337,7 @@ M.downloadFile = downloadFile
 
 -- Stops file downloading.
 -- If file already downloaded do nothing.
--- @file_id Identifier of file to cancel download By Sajad Aliraqe
+-- @file_id Identifier of file to cancel download
 local function cancelDownloadFile(file_id, dl_cb, cmd)
   tdcli_function ({
     ID = "CancelDownloadFile",
